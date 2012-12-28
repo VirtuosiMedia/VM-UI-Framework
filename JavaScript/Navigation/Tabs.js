69e3d3fae532d6e10477dd148ca7d69b09e88e87
VirtuosiMedia <contact@virtuosimedia.com>
2012-12-27 21:14:28 -0800

    Tabs now turn into accordions at mobile sizes, fixes #9

--- JavaScript/Navigation/Tabs.js
+++ JavaScript/Navigation/Tabs.js
@@ -18,34 +18,60 @@ var Tabs = new Class({
 		var self = this;
 		this.cachedTabs = [];
 		Array.each($$(selectors), function(tabMenu){
-			self.triggerTabMenu(tabMenu, selectors);
-		});
+			self.createTabMenu(tabMenu, selectors);
+			if (self.isMobile()){
+				self.setMobileTabs(tabMenu);
+			}			
+		});		
 	},
 	
-	triggerTabMenu: function(tabMenu, selectors){
-		var trigger = (tabMenu.getData('trigger')) ? tabMenu.getData('trigger') : 'click';
-		var history = (tabMenu.getData('history')) ? tabMenu.getData('history') : false;
+	createTabMenu: function(tabMenu, selectors){
+		var trigger = (tabMenu.hasData('trigger')) ? tabMenu.getData('trigger') : 'click';
+		var history = (tabMenu.hasData('history')) ? tabMenu.getData('history') : false;
 		var self = this;
 
-		$$(selectors).addEvent(trigger+':relay(li a)', function(e){
+		tabMenu.addEvent(trigger+':relay(li a)', function(e){
 			if (!history) { e.stop(); }
 			this.getParent('ul').getChildren('li a').removeClass('active');
 			this.set('class', 'active');
 			self.loadTab(this);
 		});
+
+		window.addEvent('resize', function(){
+			if (self.isMobile()){
+				self.setMobileTabs(tabMenu);
+			} else {
+				self.unsetMobileTabs();
+			}
+		});
 	},
+
+	isMobile: function(){
+		return (window.getCoordinates().width <= 768);
+	},	
 	
 	loadTab: function(tab){
-		var tabId = tab.get('href').substring(1);
-		$(tabId).getSiblings().removeClass('active');
-		$(tabId).addClass('active');
+		var tabId = tab.get('href').substring(1)
+		var tabContainer = $(tabId);
+		tabContainer.getSiblings().removeClass('active').removeClass('activeMobile');
+		
 		if (!this.cachedTabs.contains(tabId)){
 			this.loadAjaxTab(tab, tabId, tab.getData('targetUrl'));
+		}
+		
+		if (this.isMobile()){
+			tabContainer.addClass('activeMobile');
+			if ((!tab.hasData('targetUrl'))||(this.cachedTabs.contains(tabId))){
+				this.loadMobileTab(tab, tabContainer);
+			}
+		} else {
+			tabContainer.addClass('active');	
 		}
 	},
 	
 	loadAjaxTab: function(tab, tabId, targetUrl){
 		if ((targetUrl)&&(targetUrl.test(/^[^http]/))){
+			var self = this;
 			$(tabId).set('load', {
 				evalScripts: true,
 				onRequest: function(){
@@ -54,9 +80,15 @@ var Tabs = new Class({
 				onSuccess: function(responseText, responseElements, responseHTML){
 					$(tabId).set('html', responseHTML);
 					$(document.body).fireEvent('ajaxUpdate', $(tabId));
+					if (self.isMobile()){
+						self.loadMobileTab(tab, $(tabId));
+					}
 				},
 				onFailure: function(){
 					$(tabId).set('html', '<p>Tab content could not be loaded.</p>')
+					if (self.isMobile()){
+						self.loadMobileTab(tab, $(tabId));
+					}					
 				}
 			});
 			$(tabId).load(targetUrl);
@@ -68,5 +100,36 @@ var Tabs = new Class({
 			$(tabId).empty().adopt(content);
 			this.cachedTabs.push(tabId);
 		}		
+	},
+
+	loadMobileTab: function(tab, tabContainer){
+		tab.getParent().getParent().getElements('.mobileTabContent').dispose();
+		var tabContent = tabContainer.clone().addClass('mobileTabContent');
+		if (tab.getSiblings('.mobileTabContent').length == 0){
+			tabContent.inject(tab, 'after');
+		}
+		$(document.body).fireEvent('ajaxUpdate', tabContent);
+	},
+	
+	setMobileTabs: function(tabMenu){
+		this.loadTab(tabMenu.getElement('.active'));
+		$$('.tabContent, .pillTabContent').getChildren().each(function(children){
+			children.each(function(child){
+				if (child.hasClass('active') == true){
+					child.removeClass('active').addClass('activeMobile');
+				}
+			});
+		});
+	},
+	
+	unsetMobileTabs: function(){
+		$$('.tabContent, .pillTabContent').getChildren().each(function(children){
+			children.each(function(child){
+				if (child.hasClass('activeMobile')){
+					child.removeClass('activeMobile').addClass('active');
+				}
+			});
+		});
+		$$('.mobileTabContent').dispose();
 	}
 });
